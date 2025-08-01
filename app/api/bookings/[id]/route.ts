97e99587cd3c7fc8549bcbfb606bffb3f2688883
Risamaarif <risamaarif@gmail.com>
2025-08-01 22:13:47 +0700

    test

--- app/api/bookings/[id]/route.ts
+++ app/api/bookings/[id]/route.ts
@@ -1,29 +1,16 @@
 import { prisma } from "../../../../lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const id = parseInt(params.id); // pastikan id berupa number
+export async function DELETE(request: NextRequest, context: { params: { id: string } }) {
+  const id = parseInt(context.params.id);
 
   if (isNaN(id)) {
-    return new Response(JSON.stringify({ error: "Invalid ID" }), {
-      status: 400,
-    });
+    return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
   }
 
-  try {
-    await prisma.booking.delete({
-      where: { id },
-    });
+  await prisma.booking.delete({
+    where: { id },
+  });
 
-    return new Response(JSON.stringify({ message: "Booking deleted" }), {
-      status: 200,
-    });
-  } catch (error) {
-    return new Response(JSON.stringify({ error: "Failed to delete booking" }), {
-      status: 500,
-    });
-  }
+  return NextResponse.json({ message: "Booking deleted" });
 }
